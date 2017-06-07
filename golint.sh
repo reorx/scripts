381cfce65ef761577986b6dfbd555b249fe91855
reorx <novoreorx@gmail.com>
2017-06-07 20:32:47 +0800

    update golint

--- golint.sh
+++ golint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GOLINT_PATH="/Users/reorx/Code/go/bin/golint"
+#GOLINT_PATH="/Users/reorx/Code/go/bin/golint"
 EXIT_CODE=85
 if [ -z "$1" ]
 then
@@ -8,5 +8,5 @@ then
     exit $EXIT_CODE
 fi
 
-$GOLINT_PATH "$@" | egrep -v -e "(should have comment|returns unexported type)"
+$GOLINT_PATH "$@" | egrep -v -e "(should have comment|returns unexported type|func name will be used)"
 #$GOLINT_PATH "$@"
