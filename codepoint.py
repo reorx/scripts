95d4879e5036875f7010562f9794910be3e4fd3e
Reorx <novoreorx@gmail.com>
2021-11-24 18:34:59 +0800

    update codepoint

--- codepoint.py
+++ codepoint.py
@@ -1,35 +1,5 @@
-# coding: utf-8
-from __future__ import print_function
-import os
 import sys
 import subprocess as sp
-
-
-PY2 = sys.version_info.major == 2
-if not PY2:
-    unicode = str
-
-
-def b_(s):
-    """ensure binary type"""
-    if PY2:
-        if isinstance(s, unicode):
-            return s.encode('utf8')
-        return s
-    if isinstance(s, str):
-        return s.encode()
-    return s
-
-
-def t_(b):
-    """ensure text type"""
-    if PY2:
-        if isinstance(b, str):
-            return b.decode('utf8')
-        return b
-    if isinstance(b, bytes):
-        return b.decode()
-    return b
 
 
 def usage(*args):
@@ -38,45 +8,36 @@ def usage(*args):
     print('Usage: codepoint <chars>')
 
 
-if __name__ == '__main__':
+def main():
     try:
         chars = sys.argv[1]
     except IndexError:
         usage('invalid arguments, <chars> missing')
         sys.exit(1)
-    if chars == '-h':
+
+    if chars in ['-h', '--help']:
         usage()
         sys.exit()
 
-    h = [t_('Char'), t_('Ord'), t_('Hex'), t_('Code Point')]
+    h = ['Char', 'Ord', 'Hex', 'Code_Point']
     d = [h]
-    for i in t_(chars):
+    for i in chars:
         n = ord(i)
         x = hex(n)
         xs = str(x)[2:]
         if len(xs) < 4:
-            xs = t_('0') * (4 - len(xs)) + xs
-        cp = t_('U+') + xs
-        d.append([i, str(n), str(x), cp.upper()])
-    # add LF, or `column` in macos will raise `line too long` error
-    text = t_('\n').join(t_(',').join(l) for l in d) + t_('\n')
-    p = sp.Popen(
-        ['column', '-s,', '-t'],
-        stdin=sp.PIPE,
-        stdout=sp.PIPE,
-        stderr=sp.PIPE,
-        env=os.environ.copy(),
-    )
-
-    out, err = p.communicate(b_(text))
-    out = t_(out)
-    err = t_(err)
+            xs = '0' * (4 - len(xs)) + xs
+        cp = 'U+' + xs
+        d.append([i, str(n), str(x), cp])
+    text = '\n'.join(' '.join(l) for l in d) + '\n'
+    p = sp.Popen(['column', '-t'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+    out, err = p.communicate(text.encode())
     if p.returncode == 0:
-        print(out)
+        print(out.decode())
     else:
-        print(
-            t_('Out: {}\n\nErr:{}').format(
-                out, err,
-            )
-        )
+        print('Out: {}\n\nErr:{}'.format(out.decode(), err.decode()))
         sys.exit(p.returncode)
+
+
+if __name__ == '__main__':
+    main()
