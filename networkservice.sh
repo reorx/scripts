08b7d0ff24ee643a60319719b119c81a48d393bd
Reorx <novoreorx@gmail.com>
2025-07-03 18:45:48 +0800

    update networkservice to show all active devices with ip

--- networkservice.sh
+++ networkservice.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# TODO show all active
 
 services=$(networksetup -listnetworkserviceorder | grep 'Hardware Port')
 
@@ -9,21 +8,22 @@ while read line; do
     #echo "Current service: $sname, $sdev, $currentservice"
     if [ -n "$sdev" ]; then
         ifout="$(ifconfig $sdev 2>/dev/null)"
+        sip=$(echo "$ifout" | awk '/^[[:space:]]*inet /{print $2}')
+        smac=$(echo "$ifout" | awk '/ether/{print $2}')
+
         echo "$ifout" | grep 'status: active' > /dev/null 2>&1
         rc="$?"
         if [ "$rc" -eq 0 ]; then
+            echo "Active Service: $sname"
+            echo "  device: $sdev"
+            echo "  ip: $sip"
+            echo "  mac: $smac"
             currentservice="$sname"
-            currentdevice="$sdev"
-            currentmac=$(echo "$ifout" | awk '/ether/{print $2}')
         fi
     fi
 done <<< "$(echo "$services")"
 
-if [ -n "$currentservice" ]; then
-    echo $currentservice
-    echo $currentdevice
-    echo $currentmac
-else
+if [ -z "$currentservice" ]; then
     >&2 echo "Could not find current service"
     exit 1
 fi
