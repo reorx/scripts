f401fbdf21ef76647b0711449fe7dbb2befd30c1
Reorx <novoreorx@gmail.com>
2025-07-25 14:16:15 +0800

    refactor myworkspace.py to python 3 only

--- myworkspace.py
+++ myworkspace.py
@@ -1,61 +1,70 @@
-#!/usr/bin/env python
-# coding: utf-8
+#!/usr/bin/env python3
 
-import sh
 import os
+import subprocess
 import functools
 from operator import getitem
 from contextlib import contextmanager
+from pathlib import Path
+from typing import Optional, List, Dict, Any, Union
 
 
-class options:  # NOQA
+class Options:
     items_per_line = 3
     tab = 2
 
 
-def get_node_type(name):
-    if os.path.isdir(name):
+def get_node_type(name: str) -> str:
+    path = Path(name)
+    if path.is_dir():
         return 'dir'
-    elif os.path.isfile(name):
+    elif path.is_file():
         return 'file'
     else:
-        path = os.path.abspath(name)
-        raise TypeError('WTF is this: {}, {}'.format(name, os.stat(path)))
+        raise TypeError(f'Unknown file type: {name}, {path.stat()}')
 
 
 @contextmanager
-def cdctx(path):
+def cdctx(path: str):
     old_cwd = os.getcwd()
     os.chdir(path)
-    yield
-    os.chdir(old_cwd)
+    try:
+        yield
+    finally:
+        os.chdir(old_cwd)
 
 
-class Node(object):
-    def __init__(self, name):
+class Node:
+    def __init__(self, name: str):
         self.name = name
         self.type = get_node_type(name)
-        self.is_git = None
-        self.pushed = None
+        self.is_git: Optional[bool] = None
+        self.pushed: Optional[bool] = None
 
-    def _ensure_type(self, type):
-        if type != self.type:
-            raise TypeError('Should be type %s to perform' % type)
+    def _ensure_type(self, node_type: str) -> None:
+        if node_type != self.type:
+            raise TypeError(f'Should be type {node_type} to perform this operation')
 
-    def check_git(self):
+    def check_git(self) -> None:
         self._ensure_type('dir')
         with cdctx(self.name):
-            if os.path.exists('.git'):
+            if Path('.git').exists():
                 self.is_git = True
-                uncleared = sh.git('status', '--porcelain').strip()
-                if uncleared:
+                try:
+                    result = subprocess.run(
+                        ['git', 'status', '--porcelain'],
+                        capture_output=True,
+                        text=True,
+                        check=True
+                    )
+                    uncleared = result.stdout.strip()
+                    self.pushed = not bool(uncleared)
+                except subprocess.CalledProcessError:
                     self.pushed = False
-                else:
-                    self.pushed = True
             else:
                 self.is_git = False
 
-    def __str__(self):
+    def __str__(self) -> str:
         is_git = 'git' if self.is_git is True else ''
         if self.pushed is True:
             pushed = 'pushed'
@@ -63,15 +72,12 @@ class Node(object):
             pushed = 'not pushed'
         else:
             pushed = ''
-        return '<{type} {name}, {is_git}, {pushed}>'.format(
-            type=self.type, name=self.name,
-            is_git=is_git, pushed=pushed
-        )
+        return f'<{self.type} {self.name}, {is_git}, {pushed}>'
 
 
-class NodeList(object):
-    def __init__(self, *paths):
-        nodes = {}
+class NodeList:
+    def __init__(self, *paths: str):
+        nodes: Dict[str, Any] = {}
         for path in paths:
             segments = path.split('/')
             last_pos = len(segments) - 1
@@ -81,58 +87,60 @@ class NodeList(object):
                     last_item = last_item.setdefault(segment, {})
                 else:
                     last_item.setdefault(segment, [])
-        #print(nodes)
         self.nodes = nodes
 
     @staticmethod
-    def _getitem_by_path(d, path):
+    def _getitem_by_path(d: Dict[str, Any], path: str) -> Any:
         if not path:
             return d
         return functools.reduce(lambda x, y: getitem(x, y), [d] + path.split('/'))
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> Any:
         return self._getitem_by_path(self.nodes, key)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: Any) -> None:
         i = key.rfind('/')
         a = key[:i]
         b = key[i + 1:]
         self[a][b] = value
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: str) -> None:
         pass
 
-    def len(self, path):
+    def len(self, path: str) -> int:
         v = self[path]
         if isinstance(v, dict):
             length = 0
             for k in v:
-                length += self.len(path + '/' + k)
+                length += self.len(f'{path}/{k}')
         else:
             length = len(v)
         return length
 
 
-def list_nodes():
-    for i in sh.ls('-1').split('\n'):
-        name = i.strip()
-        if not name:
-            continue
-        yield Node(name)
+def list_nodes() -> List[Node]:
+    nodes = []
+    try:
+        for item in os.listdir('.'):
+            if item.strip():
+                nodes.append(Node(item))
+    except OSError:
+        pass
+    return nodes
 
 
-def echo(s, indent=None, prefix=None):
+def echo(s: str, indent: Optional[int] = None, prefix: Optional[str] = None) -> None:
     if prefix:
         s = prefix + s
     if indent:
         s = ' ' * indent + s
-
     print(s)
 
 
-def echo_nodes(nodes, indent=None, prefix='│ '):
+def echo_nodes(nodes: List[Node], indent: Optional[int] = None, prefix: str = '│ ') -> None:
+    options = Options()
     lines = []
-    col_max_widths = [0 for x in range(options.items_per_line)]
+    col_max_widths = [0 for _ in range(options.items_per_line)]
     line_buf = []
 
     for i, node in enumerate(nodes):
@@ -144,8 +152,6 @@ def echo_nodes(nodes, indent=None, prefix='│ '):
             col_max_widths[pos] = text_len
 
         if len(line_buf) >= options.items_per_line:
-            #line = '\t'.join(line_buf)
-            #echo(line, indent, prefix)
             lines.append(line_buf)
             line_buf = []
 
@@ -156,23 +162,15 @@ def echo_nodes(nodes, indent=None, prefix='│ '):
 
     # Echo lines
     padding = 2
-    line_tpl = ''.join('{:<%s}' % (i + padding) for i in col_max_widths)
+    line_tpl = ''.join(f'{{:<{i + padding}}}' for i in col_max_widths)
     for line in lines:
         line_str = line_tpl.format(*line)
         echo(line_str, indent, prefix)
 
-    #col_width = max(len(word) for line in lines for word in line) + 2  # 2 is padding
-    #for line in lines:
-    #    line_str = "".join(word.ljust(col_width) for word in line)
-    #    echo(line_str, indent, prefix)
 
-    #widths = [max(map(len, col)) for col in zip(*lines)]
-    #for row in table:
-        #print('  '.join((val.ljust(width) for val, width in zip(row, widths))))
-
-
-def main():
-    echo('cwd: {}'.format(os.getcwd()))
+def main() -> None:
+    options = Options()
+    echo(f'cwd: {os.getcwd()}')
     echo('Scanning..\n')
     nodes = NodeList(
         'file',
@@ -180,16 +178,6 @@ def main():
         'dir/git/_',
         'dir/git/pushed',
     )
-    #nodes = {
-    #    'file': [],
-    #    'dir': {
-    #        'git': {
-    #            'pushed': [],
-    #            '_': []
-    #        },
-    #        '_': []
-    #    }
-    #}
 
     for node in list_nodes():
         if node.type == 'dir':
@@ -198,13 +186,10 @@ def main():
             if node.is_git:
                 if node.pushed:
                     nodes['dir/git/pushed'].append(node)
-                    #nodes['dir']['git']['pushed'].append(node)
                 else:
                     nodes['dir/git/_'].append(node)
-                    #nodes['dir']['git']['_'].append(node)
             else:
                 nodes['dir/_'].append(node)
-                #nodes['dir']['_'].append(node)
         else:
             nodes['file'].append(node)
 
@@ -212,27 +197,25 @@ def main():
     tab = options.tab
 
     k = 'file'
-    echo('Files (%s):' % nodes.len(k))
+    echo(f'Files ({nodes.len(k)}):')
     echo_nodes(nodes[k], tab)
     echo('')
 
-    #
-    echo('Directories (%s):' % nodes.len('dir'))
-    # #
-    echo('Git (%s):' % nodes.len('dir/git'), tab)
-    # ##
+    echo(f'Directories ({nodes.len("dir")}):')
+    echo(f'Git ({nodes.len("dir/git")}):', tab)
+    
     k = 'dir/git/pushed'
-    echo('Pushed (%s):' % nodes.len(k), tab * 2)
+    echo(f'Pushed ({nodes.len(k)}):', tab * 2)
     echo_nodes(nodes[k], tab * 2)
     echo('')
-    # ##
+    
     k = 'dir/git/_'
-    echo('Not pushed (%s):' % nodes.len(k), tab * 2)
+    echo(f'Not pushed ({nodes.len(k)}):', tab * 2)
     echo_nodes(nodes[k], tab * 2)
     echo('')
-    # #
+    
     k = 'dir/_'
-    echo('Normal (%s):' % nodes.len(k), tab)
+    echo(f'Normal ({nodes.len(k)}):', tab)
     echo_nodes(nodes[k], tab)
 
 
