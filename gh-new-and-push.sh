886346ca6b50d248b156bedc79b7eab152cd24a9
Reorx <novoreorx@gmail.com>
2025-11-30 10:33:40 +0800

    update gh-new-and-push.sh

--- gh-new-and-push.sh
+++ gh-new-and-push.sh
@@ -4,9 +4,13 @@ set -eu
 
 username=reorx
 reponame=$(basename "$PWD")
-set -x
 
-gh repo create "$reponame" --private
+if [ -n "$SKIP_CREATE" ]; then
+    echo "skip creating the repo"
+else
+    gh repo create "$reponame" --private
+fi
+set -x
 
 git remote add origin "git@github.com:$username/$reponame.git"
 git push -u origin master
